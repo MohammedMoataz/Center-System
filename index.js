652510936b1f705bc0e3f5848278c09293b781e1
Mohammed Moataz <imohammedmoataz@gmail.com>
2023-08-25 11:10:48 +0300

    made it easy to access swagger documentation

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ import swaggerJsdoc from 'swagger-jsdoc'
 import swaggerUi from 'swagger-ui-express'
 import { config } from 'dotenv'
 
-import { options } from './utils/constants.js'
 import AdminRoutes from './routes/admin.route.js'
 import StudentRoutes from './routes/student.route.js'
+import { options } from './utils/constants.js'
 
 config()
 const PORT = process.env.PORT || 8000
@@ -23,6 +23,7 @@ app.use(json())
 app.use(express.urlencoded({ extended: false }))
 
 // Routes
+app.get('/', (req, res) => res.redirect(`http://localhost:${PORT}/api-docs`))
 app.use('/admin', AdminRoutes)
 app.use('/student', StudentRoutes)
 app.use(
@@ -32,13 +33,16 @@ app.use(
 )
 
 // Running server
-app.listen(PORT, () => console.log(`Server is Listening on http://localhost:${PORT}`))
+app.listen(PORT, () => {
+    console.info(`Server is running on http://localhost:${PORT}`)
+    console.info(`for swagger documentation you can visit http://localhost:${PORT}/api-docs`)
+})
 
 // Global Error Handler. IMPORTANT function params MUST start with err
 app.use((err, req, res, next) => {
-    console.log(err.stack)
-    console.log(err.name)
-    console.log(err.code)
+    console.error(err.stack)
+    console.error(err.name)
+    console.error(err.code)
 
     res.status(500).json({
         message: err.message,
